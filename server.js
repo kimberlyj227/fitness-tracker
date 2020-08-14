be08a3145d1d0558ddd3610b2b0b70a5e7bc0f99
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-08-14 00:54:26 -0600

    updated server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", {
 
 require("./routes/htmlRoutes.js")(app);
 require("./routes/apiRoutes.js")(app);
-// require("./seeders/seed.js");
+require("./seeders/seed.js");
 
 
 
